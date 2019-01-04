af31b66aca402adc313a10685b70cb7c83495592
Alexandre Pereira <alexandre.pereira@webtraining.int>
2019-01-04 09:04:01 -0200

    Adicionado classe de conversão de moeda, desenvolvido em conjunto com a parte 6 do curso de CSharp da Alura

--- BankNumbers/BankFive/Program.cs
+++ BankNumbers/BankFive/Program.cs
@@ -13,6 +13,19 @@ namespace BankFive
             ContaCorrente conta = new ContaCorrente(007, 12345);
 
             Console.WriteLine(conta.Numero);
+
+
+            ConversorMoeda converter = new ConversorMoeda("https://www.banknumbers.com/conversor?moedaDestino=real&moedaOrigem=dolar&valor=500");
+
+            string moedaOrigem = converter.GetValor("moedaOrigem");
+            Console.WriteLine("Moeda de origem: " + moedaOrigem);
+
+            string moedaDestino = converter.GetValor("moedaDestino");
+            Console.WriteLine("Moeda de destino: " + moedaDestino);
+
+            string valor = converter.GetValor("valor");
+            Console.WriteLine("Valor a converter: " + valor);
+
             Console.ReadLine();
         }
     }
---
+++ BankNumbers/Modelos/ConversorMoeda.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankFive
+{
+    /// <summary>
+    /// Receber URL com parametros e retornar os valores de moedas a serem calculados 
+    /// </summary>
+    public class ConversorMoeda
+    {
+        /// <summary>
+        /// URL recebida pelo construtor
+        /// </summary>
+        public string URL { get; }
+
+        private readonly string _argumentos;
+
+        /// <summary>
+        /// Construtor que recebe uma URL válida e separa os argumentos desejados
+        /// </summary>
+        /// <param name="url">URL válida contendo os parametros necessários para o cálculo de conversão.</param>
+        public ConversorMoeda(string url)
+        {
+            URL = url;
+
+            int indiceInterrogacao = URL.IndexOf("?");
+            _argumentos = URL.Substring(indiceInterrogacao + 1);
+        }
+
+        /// <summary>
+        /// Recebe o parametro e retorna o valor correspondente
+        /// </summary>
+        /// <param name="parametro">parametro da URL do qual deseja o valor.</param>
+        /// <returns></returns>
+        public string GetValor(string parametro)
+        {
+            string paramCaixaAlta = parametro.ToUpper();
+            string argumCaixaAlta = _argumentos.ToUpper();
+
+            string termoBusca = paramCaixaAlta + "=";
+            int indiceTermo = argumCaixaAlta.IndexOf(termoBusca);
+
+            string resultado = argumCaixaAlta.Substring(indiceTermo + termoBusca.Length);
+            int indiceEComercial = resultado.IndexOf("&");
+
+            if (indiceEComercial == -1)
+            {
+                return resultado;
+            }
+
+            return resultado.Remove(indiceEComercial);
+        }
+
+    }
+}
